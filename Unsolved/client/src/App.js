8535f6cb77b6e871de23ff380c68b4b965cb9d59
Thomas Bentley <37708538+bentleygtr@users.noreply.github.com>
2018-09-24 21:05:37 -0500

    adding the files

---
+++ Unsolved/client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BrowseRouter as Router, Route, Switch, Redirect } from "react-router-dom"; 
+
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+      <Route exact path="/" component={Books} />
+      <Route exact path="/books" component={Books} />
+      <Route exact path="/:id" component={Detail} />
+      <Route component={NoMatch}/>
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
